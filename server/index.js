ae5025a90207314b2871f6aab0020e59a29ff81d
Aman20-debug <amanpoddar08072003@email.com>
2025-02-08 15:09:03 +0530

    Added index.js file to server

---
+++ server/index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const app = express();
+
+const userRoutes = require("./routes/User");
+const profileRoutes = require("./routes/Profile");
+const paymentRoutes = require("./routes/Payments");
+const courseRoutes = require("./routes/Course");
+const contactUsRoute = require("./routes/Contact");
+const database = require("./config/database");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const {cloudinaryConnect } = require("./config/cloudinary");
+const fileUpload = require("express-fileupload");
+const dotenv = require("dotenv");
+
+// console.log("Cloudinary Credentials:", {
+//     CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+//     API_KEY: process.env.CLOUDINARY_API_KEY,
+//     API_SECRET: process.env.CLOUDINARY_API_SECRET,
+//     FOLDER_NAME: process.env.FOLDER_NAME
+// });
+
+
+//load config from env file
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+
+//connect to the database
+const dbconnect = require("./config/database");
+dbconnect();
+
+
+//Middleware 
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+    cors({
+        origin: "https://localhost:3000",
+        credentials: true,
+    })
+)
+
+//middleware for uploading file into our server
+const fileupload = require("express-fileupload");
+app.use(fileupload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/'
+})); 
+
+
+//mount the API routes
+app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/profile", profileRoutes);
+app.use("/api/v1/course", courseRoutes);
+app.use("/api/v1/payment", paymentRoutes);
+
+//start server
+app.listen(PORT, () => {
+    console.log(`Server Started Successfuly at, ${PORT}`);
+})
+
+
+//default route
+app.get("/", (req, res) => {
+    res.send(`<h1> This is HOMEPAGE Aman</h1>`);
+})
