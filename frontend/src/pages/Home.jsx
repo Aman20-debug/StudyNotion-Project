6c54d10648248e79788d9b75073bd66b66536667
Aman20-debug <amanpoddar08072003@email.com>
2025-02-12 18:10:54 +0530

    added frontend

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { FaArrowRight } from "react-icons/fa";
+import {Link} from "react-router-dom";
+
+export const Home = () => {
+  return (
+    <div>
+
+        {/* Section 1 */}
+
+        <div>
+
+            <Link to={"/signup"}>
+                <div>
+                    <div>
+                        <p>Become an Instructor</p>
+                        <FaArrowRight/>
+                    </div>
+                </div>
+            </Link>
+
+        </div>        
+
+
+        {/* Section 2 */}
+
+
+        {/* Section 3 */}
+
+        {/* Footer */}
+
+    </div>
+  )
+}
