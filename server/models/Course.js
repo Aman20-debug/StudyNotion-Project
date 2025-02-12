4f8d7f0168178afb81fdd9f84a4db140efeb0e88
Aman20-debug <amanpoddar08072003@email.com>
2025-02-12 18:27:02 +0530

    started tracking untracked folder models

---
+++ server/models/Course.js
@@ -0,0 +1,80 @@
+//Model 3
+const mongoose = require("mongoose");
+
+const Course = new mongoose.Schema({
+    courseName: {
+        type: String,
+    },
+
+    courseDescription: {
+        type: String,
+    },
+
+    instructor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+
+    whatYouWillLearn: {
+        type: String,
+    },
+
+    courseContent: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Section",
+        }
+    ],
+
+    ratingAndReviews: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "RatingAndReview",
+        }
+    ],
+
+    price: {
+        type: Number,
+    },
+
+    thumbNail: {
+        type: String,
+    },
+
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+    },
+
+    tags: {
+        type: String,
+        required: true,
+    },
+
+    studentsEnrolled: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        }
+    ],
+
+    instructions: {
+		type: [String],
+	},
+
+    status: {
+		type: String,
+		enum: ["Draft", "Published"],
+	},
+
+    createdAt: {
+		type:Date,
+		default:Date.now
+	},
+
+
+});
+
+module.exports = mongoose.model("Course", Course);
