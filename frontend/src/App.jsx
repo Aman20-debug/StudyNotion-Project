fb65c61f1d6ebbffa7fec2e69812d86350532cb5
Aman20-debug <amanpoddar08072003@email.com>
2025-03-20 15:37:01 +0530

    Adding Home Route to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,23 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import {Home} from "./pages/Home";
+import Navbar from "./components/common/Navbar"
+
+import OpenRoute from "./components/core/Auth/OpenRoute"
+
+
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 
 function App() {
 
   return (
-    <div>
+    <div className='w-screen min-h-screen bg-richblack-900 flex flex-col font-inter'>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>} />
-      </Routes>
+        
+      </Routes> 
     </div>
   )
 }
