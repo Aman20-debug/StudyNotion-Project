91ee811a3fc1ac48674808268e5313b536c56291
Aman20-debug <amanpoddar08072003@gmail.com>
2025-06-27 00:43:08 +0530

    Updated BACKEND_URL

---
+++ frontend/src/utils/url.jsx
@@ -0,0 +1,4 @@
+//const VITE_BACKEND_URL = "http://localhost:4000/api/v1";
+// or production
+const VITE_BACKEND_URL = "https://studynotion-backend.onrender.com/api/v1";
+export default VITE_BACKEND_URL;
