92a27b5789724cf224110b6ea58aa8175ee83f5d
Aman20-debug <amanpoddar08072003@gmail.com>
2025-06-27 00:22:06 +0530

    Corrected Frontend Url

--- server/utils/url.js
+++ server/utils/url.js
@@ -1,4 +1,4 @@
 
-//const FRONTEND_URL = "https://studynotion-f2.onrender.com"
-const FRONTEND_URL = "http://localhost:5173"
+const FRONTEND_URL = "https://studynotion-f2.onrender.com"
+// const FRONTEND_URL = "http://localhost:5173"
 module.exports = FRONTEND_URL;
